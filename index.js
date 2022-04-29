7a4b19db7060fd9cfb14f33bb7ec3cfc3e1cf94f
SevroAuBarca <daniel_contreras001@hotmail.com>
2022-04-29 13:49:31 -0500

    Advice generator app challenge Done

---
+++ index.js
@@ -0,0 +1,24 @@
+const adviceNumber = document.getElementById("advice-number");
+const adviceQuote = document.getElementById("advice-quote");
+const changeQuoteButton = document.querySelector(".advice-button");
+
+document.addEventListener("DOMContentLoaded", () => {
+  init();
+});
+
+changeQuoteButton.addEventListener("click", () => {
+  init();
+});
+
+const getQuote = async () => {
+  const data = await fetch("https://api.adviceslip.com/advice");
+  return data.json();
+};
+
+const init = async () => {
+  const data = await getQuote();
+  adviceNumber.innerText = data.slip.id;
+  adviceQuote.innerHTML = String.raw`
+    <span class="advice-quote" id="advice-quote">"${data.slip.advice}"</span>
+  `;
+};
